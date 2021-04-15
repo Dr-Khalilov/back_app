e69b571786f3e68ed1f24ebd8542b42da19f73da
Umar Khalilov <ERMASTER100@gmail.com>
2021-04-15 10:00:35 +0300

    Add define model and migration

---
+++ migrations/20210415063831-create-todo.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      body: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      deadline: {
+        allowNull: false,
+        type: Sequelize.DATEONLY,
+      },
+      isDone: {
+        field: 'is_done',
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        field: 'created_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        field: 'updated_at',
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('todos');
+  },
+};
---
+++ models/todo.js
@@ -0,0 +1,43 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    static associate (models) {}
+  }
+  Todo.init(
+    {
+      body: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      deadline: {
+        allowNull: false,
+        type: DataTypes.DATEONLY,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      isDone: {
+        field: 'is_done',
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Todo',
+      tableName: 'todos',
+      underscored: true,
+    }
+  );
+  return Todo;
+};
